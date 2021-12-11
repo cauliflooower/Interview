0deaad85bbc5085295b36308a0d4e25ccb808a16
Angelina <86556425+cauliflooower@users.noreply.github.com>
2021-12-11 20:33:06 +0300

    'feat: add solution'

---
+++ app.js
@@ -0,0 +1,20 @@
+function binarySearch(value, list) {
+  let first = 0; 
+  let last = list.length - 1;
+  let position = -1;
+  let found = false;
+  let middle;
+
+  while (found === false && first <= last) {
+      middle = Math.floor((first + last)/2);
+      if (list[middle] == value) {
+          found = true;
+          position = middle;
+      } else if (list[middle] > value) { 
+          last = middle - 1;
+      } else {
+          first = middle + 1;
+      }
+  }
+  return position;
+}
